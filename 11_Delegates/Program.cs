343c7f34846d99cc1cf898fbac42d9b2323f0600
dev <dev@A21PC6>
2025-07-02 17:55:52 +0300

    Task 1-2 tp/do

---
+++ 11_Delegates/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _11_Delegates
+{
+    class Arr
+    {
+        int[] arr;
+        public Arr()
+        {
+            arr = new int[5] {1,56,7,5,4 };
+        }
+        public int countN()
+        {
+            int Negative = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i]<0)
+                {
+                    Negative++
+                }
+            }
+            return Negative;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
