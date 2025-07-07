9b585f18a9785e6b4baa879137ee97bd2f5c4c60
dev <dev@A21PC6>
2025-07-07 17:53:03 +0300

    Task 1-3 to/do

---
+++ 14_Dictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _14_Dictionary
+{
+    class Abonent
+    {
+        public int phone { get; set; }
+        public string Name { get; set; }
+    }
+    class PhoneBook
+    {
+
+        Dictionary<int, Abonent> phones;
+
+        public void AddNumber(int id, Abonent ab)
+        {
+            if (phones[id] != null)
+            {
+                throw new Exception("Value is Reserved");
+            }
+            else { phones[id] = ab; }
+        }
+        public void RemoveNumber(int id) {
+            for (int i = 0; i < phones.Count; i++) { 
+                if (phones[i].phone == id) { phones.Remove(); }
+            }
+        }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //phones
+        }
+    }
+}
