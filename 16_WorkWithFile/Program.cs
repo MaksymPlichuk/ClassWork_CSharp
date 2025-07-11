6b1269afb7eca5ac11fead345c07e797bd1fd402
dev <dev@A21PC6>
2025-07-11 17:56:55 +0300

    Tasks 2-3 to/do

---
+++ 16_WorkWithFile/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace _16_WorkWithFile
+{
+    internal class Program
+    {
+        static void WriteFile(string path)
+        {
+
+            using (FileStream fs = new FileStream(path, FileMode.Create,
+                FileAccess.Write, FileShare.None))
+            {
+
+                Console.WriteLine("Enter elements of array : ");
+                string writeText = "";
+                for (int i = 0; i < 5; i++) 
+                {
+                    writeText += Console.ReadLine()!;
+                }
+
+                byte[] writeBites = Encoding.Default.GetBytes(writeText);
+                fs.Write(writeBites, 0, writeBites.Length);
+
+                Console.WriteLine("File was recorded!!!!");
+
+            }
+
+        }
+        static string ReadFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                return File.ReadAllText(path);
+            }
+            else { throw new Exception("Wrong file path!"); }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
