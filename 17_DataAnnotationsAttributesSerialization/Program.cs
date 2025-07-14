8c01610a7f8d7b5bd1d7c5eda1461dfc0ccc0920
dev <dev@A21PC6>
2025-07-14 17:54:59 +0300

    Tasks 1-6 to/do

---
+++ 17_DataAnnotationsAttributesSerialization/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _17_DataAnnotationsAttributesSerialization
+{
+    class User
+    {
+        [Required]
+        [Range(1000, 9999,ErrorMessage = "Wrong ID")]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Name not setted")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Wrong lenght")]
+        public string Login { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
