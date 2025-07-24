e0ec6281998ecf4792c888079de4ba915fdf6066
Maksym <pilipm449@gmail.com>
2025-07-24 16:19:07 +0300

    Tasks 1,2,5 done HW

--- 18_RegularExpression/Program.cs
+++ 18_RegularExpression/Program.cs
@@ -25,27 +25,62 @@ namespace _18_RegularExpression
             foreach (Match text in m)
             {
                 Console.Write(text.Value + " ");
-                floatNums[i] = float.Parse(text.Value);
+            }
+            Console.WriteLine("\n");
+            foreach (Match text in m)
+            {
+                try
+                {
+                    floatNums[i] = float.Parse(text.Value);
+                }
+                catch
+                {
+                    Console.WriteLine($"Couldn't convert {text} to int :( ");
+                }
                 i++;
             }
 
             Console.WriteLine("\n\tTo ints array: ");
+
+            for (int j = 0; j < floatNums.Length; j++)
             {
-                for (int j = 0; j < floatNums.Length; j++)
-                {
-                    Console.Write(floatNums[j] + " ");
-                }
-                
+                Console.Write(floatNums[j] + " ");
             }
+            Console.WriteLine();
+
+        }
+        static void UserReg()
+        {
+            Console.Write("\nEnter email: "); string email = Console.ReadLine()!;
+            string pattern = @"^[a-zA-Z0-9-._]{4,}\@[a-z-A-Z0-9]{2,}\.[a-zA-Z0-9]+$";
+            Regex regex = new Regex(pattern);
+
+            if (!regex.IsMatch(email)) { 
+                Console.WriteLine("\nWrong email!");
+                return; 
+            }
+
+            Console.Write("Enter password: "); string password = Console.ReadLine()!;
+            string pattern2 = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[-_])[a-zA-Z0-9-_]{6,}$";
+
+            regex = new Regex(pattern2);
+            if (!regex.IsMatch(password)) { 
+                Console.WriteLine("\nWrong password!"); 
+                return; 
+            }
+
+            Console.WriteLine($"\nEmail is correct: {email}");
+            Console.WriteLine($"Password is correct: {password}");
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Task 1");
+            Console.WriteLine("Task 1-2");
             string path = Path.Combine(Directory.GetCurrentDirectory(), "numbers.txt");
             WriteToFile(path);
             FindFloat(path);
 
-            
+            Console.WriteLine("\nTask 5");
+            UserReg();
         }
     }
 }
