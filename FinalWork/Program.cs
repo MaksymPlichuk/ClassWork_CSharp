7c57abdaff26705d4d7c5eb431e149d9863820f5
dev <dev@A21PC6>
2025-08-11 17:45:10 +0300

    to/do

---
+++ FinalWork/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FinalWork
+{
+    class Dictionary
+    {
+        Dictionary<string, string> dict = new Dictionary<string, string>();
+        public void AddTranslation(string key, string value)
+        {
+            dict[key] = value;
+        }
+        public void ReplaceTranslation() { }
+        public void DeleteTranslation(string key) { }
+
+        public void FindTranslation(string key) { }
+
+        public void SaveDictionary() { }
+        public void LoadDictionary() { }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter operation");
+            while (true)
+            {
+
+            }
+        }
+    }
+}
